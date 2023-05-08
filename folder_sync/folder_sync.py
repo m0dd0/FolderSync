6d34f62ade9f262275edf1adc70a27eb0d25c37f
Moritz Hesche <mo.hesche@gmail.com>
2023-05-08 14:57:35 +0200

    action collections

--- folder_sync/folder_sync.py
+++ folder_sync/folder_sync.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Callable
+from typing import List, Callable, Tuple, Dict, Union, Any
 import hashlib
 import logging
 import concurrent.futures
@@ -7,87 +7,64 @@ import os
 import shutil
 from collections import defaultdict
 import time
+import filecmp
+import enum
 
 from tqdm import tqdm
 
 
-def metadata_hash(file_path: Path) -> str:
-    """Hash files based on their modification stamp and file name and size.
-
-    Args:
-        file_path: path to the file
-
-    Returns:
-        "hash" of the file metadata
-    """
-    # NOTE: it makes no sense to hash on all attributes of path.stat() as the inode number and pther fields are not preserved when copying the file
-    return (
-        f"{file_path.name}_{os.path.getmtime(file_path)}_{os.path.getsize(file_path)}"
-    )
+class Action(enum.Enum):
+    COPY_FILE = "copy_file"
+    DELETE_FILE = "delete_file"
+    DELETE_EMPTY_FOLDER = "delete_folder"
+    CREATE_EMPTY_FOLDER = "create_folder"
+    COMPARE_FILE = "compare_file"
 
 
-def content_hash(
-    file_path: Path, hash_func: Callable = hashlib.md5, block_size: int = 65536
-) -> str:
-    """Return the md5 hash of a file considering its content only.
-
-    Args:
-        file_path: path to the file
-        hash_func: hash function to use, default is md5
-        block_size: size of the block to read from the file at once, default is 65536
-
-    Returns:
-        md5 hash of the file content
-    """
-    hasher = hash_func()
-
-    with open(file_path, "rb") as file:
-        while True:
-            data = file.read(block_size)
-            if not data:
-                break
-            hasher.update(data)
-
-    content_hash = hasher.hexdigest()
-
-    return content_hash
+class Stat(enum.Enum):
+    DELETED_FOLDERS = "deleted_folders"
+    DELETED_FILES = "deleted_files"
+    COPIED_FOLDERS = "copied_folders"
+    COPIED_FILES = "copied_files"
+    COMPARED_FILES = "compared_files"
+    CHANGED_FILES = "changed_files"
+    UNCHANGED_FILES = "unchanged_files"
 
 
-def _delete_outdated_element(target_path: Path):
-    if target_path.is_file():
-        target_path.unlink()
-        return "deleted_file"
-    elif target_path.is_dir():
-        shutil.rmtree(target_path)
-        return "deleted_folder"
-
-
-def _copy_new_element(source_path: Path, target_path: Path):
-    if source_path.is_file():
-        shutil.copy2(source_path, target_path)
-        return "copied_file"
-    elif source_path.is_dir():
-        # TODO do own recursion for better tracking of progress, !!! accoutn for empty folders
-        shutil.copytree(source_path, target_path)
-        return "copied_folder"
-
-
-def _update_existing_file(
-    source_path: Path, target_path: Path, file_hash_func: Callable
+def _get_delete_folder_actions(
+    folder_path: Path, actions: Dict[Action, List[Any]], stats: Dict[Stat, List[Any]]
 ):
-    if file_hash_func(source_path) == file_hash_func(target_path):
-        return "unchanged_file"
-    target_path.unlink()
-    shutil.copy2(source_path, target_path.parent)
-    return "changed_file"
+    for p in folder_path.iterdir():
+        if p.is_dir():
+            _get_delete_folder_actions(p)
+        elif p.is_file():
+            actions[Action.DELETE_FILE].append(p)
+            stats[Stat.DELETED_FILES].append(p)
+
+    actions[Action.DELETE_EMPTY_FOLDER].append(folder_path)
 
 
-def _sync_dir(
+def _get_copy_folder_actions(
+    source_path,
+    target_path,
+    actions: Dict[Action, List[Any]],
+    stats: Dict[Stat, List[Any]],
+):
+    actions[Action.CREATE_EMPTY_FOLDER].append(target_path)
+
+    for p in source_path.iterdir():
+        if p.is_dir():
+            _get_copy_folder_actions(p, target_path / p.name, actions)
+        elif p.is_file():
+            actions[Action.COPY_FILE].append((p, target_path / p.name))
+            stats[Stat.COPIED_FILES].append((p, target_path / p.name))
+
+
+def _get_actions(
     source_dir: Path,
     target_dir: Path,
-    file_hash_func: Callable,
-    executer: concurrent.futures.ThreadPoolExecutor,
-    futures: List,
+    actions: Dict[Action, List[Any]],
+    stats: Dict[Stat, List[Any]],
 ):
     target_names = [p.name for p in target_dir.iterdir()]
     source_names = [p.name for p in source_dir.iterdir()]
@@ -103,7 +80,12 @@ def _sync_dir(
             and source_path.is_dir() == target_path.is_dir()
             and source_path.exists() == target_path.exists()
         ):
-            futures.append(executer.submit(_delete_outdated_element, target_path))
+            if target_path.is_file():
+                stats[Stat.DELETED_FILES].append(target_path)
+                actions[Action.DELETE_FILE].append(target_path)
+            elif target_path.is_dir():
+                stats[Stat.DELETED_FOLDERS].append(target_path)
+                _get_delete_folder_actions(target_path, actions)
         else:
             kept_target_names.append(name)
 
@@ -115,24 +97,40 @@ def _sync_dir(
         target_path = target_dir / name
 
         if name not in target_names:
-            futures.append(executer.submit(_copy_new_element, source_path, target_path))
+            if source_path.is_file():
+                actions[Action.COPY_FILE].append((source_path, target_path))
+                stats[Stat.COPIED_FILES].append((source_path, target_path))
+            elif source_path.is_dir():
+                stats[Stat.COPIED_FOLDERS].append((source_path, target_path))
+                _get_copy_folder_actions(source_path, target_path, actions)
 
         else:  # name exists in source and target and is of same type (file or folder)
             if source_path.is_file():
-                futures.append(
-                    executer.submit(
-                        _update_existing_file, source_path, target_path, file_hash_func
-                    )
-                )
+                stats[Stat.COMPARED_FILES].append((source_path, target_path))
+                actions[Action.COMPARE_FILE].append((source_path, target_path))
             elif source_path.is_dir():
-                _sync_dir(source_path, target_path, file_hash_func, executer, futures)
+                _get_actions(source_path, target_path, actions)
+
+
+def _run_executer_with_progress(n_threads, func, data: List[Tuple]):
+    executer = concurrent.futures.ThreadPoolExecutor(max_workers=n_threads)
+    futures = [executer.submit(func, *d) for d in data]
+
+    # logging progress
+    with tqdm(total=len(futures)) as pbar:
+        for _ in concurrent.futures.as_completed(futures):
+            pbar.update(1)
+
+    executer.shutdown(wait=True)
+    return [f.result() for f in futures]
 
 
 def sync_folders(
     source_folder: Path,
     target_folder: Path,
     n_thredas: int = 1,
-    hash_func: Callable = metadata_hash,
+    shallow_comparison: bool = True,
+    verbose: bool = False,
 ):
     """Sync two folders recursively.
     All files and folders which are in target but not in source anymore are deleted.
@@ -143,30 +141,121 @@ def sync_folders(
         source_folder: path to the source folder
         target_folder: path to the target folder
         n_thredas: number of threads to use, default is 1
-        hash_func: function to use to hash files, default is metadata_hash
     """
     logging.info(f"Syncing {source_folder} to {target_folder}")
 
     start_time = time.time()
-    stats = defaultdict(int)
 
+    logging.info("Detecting necessary actions...")
+    actions = defaultdict(list)
+    stats = defaultdict(list)
+    _get_actions(source_folder, target_folder, actions, stats)
+
+    logging.info("Comparing files...")
+    results = _run_executer_with_progress
+    (
+        n_thredas,
+        filecmp.cmp,
+        actions[Action.COMPARE_FILE],
+        shallow_comparison,
+    )
+    # executer = concurrent.futures.ThreadPoolExecutor(max_workers=n_thredas)
+    # futures = [
+    #     executer.submit(filecmp.cmp, f1, f2, shallow_comparison)
+    #     for f1, f2 in actions[Action.COMPARE_FILE]
+    # ]
+
+    # # logging progress
+    # with tqdm(total=len(futures)) as pbar:
+    #     for _ in concurrent.futures.as_completed(futures):
+    #         pbar.update(1)
+
+    # update actions
+    for i, (source, target) in enumerate(actions.pop(Action.COMPARE_FILE)):
+        if not futures[i].result():
+            actions[Action.DELETE_FILE].append(target)
+            actions[Action.COPY_FILE].append((source, target))
+            stats[Stat.CHANGED_FILES].append((source, target))
+        else:
+            stats[Stat.UNCHANGED_FILES].append((source, target))
+
+    logging.info("The following actions will be executed:")
+    for action, args in actions.items():
+        logging.info(f"{action}: {args if verbose else len(args)}")
+
+    # Do you want to continue?
+    # TODO
+
+    logging.info("Executing actions...")
+    logging.info("Deleting files...")
     executer = concurrent.futures.ThreadPoolExecutor(max_workers=n_thredas)
-    futures = []
-
-    logging.info("Detecting changes...")
-    _sync_dir(source_folder, target_folder, hash_func, executer, futures)
-
-    logging.info("Applying changes...")
+    futures = [
+        executer.submit(lambda p: p.unlink(), p) for p in actions[Action.DELETE_FILE]
+    ]
     with tqdm(total=len(futures)) as pbar:
-        for future in concurrent.futures.as_completed(futures):
+        for _ in concurrent.futures.as_completed(futures):
             pbar.update(1)
-            result = future.result()
-            stats[result] += 1
 
-    executer.shutdown(wait=True)  # should be shutdown already but just to be sure
+    # delete files from target
+    # delete empty folders from target
+    # create empty folders in target
+    # copy files to target
 
     logging.info(
         f"Finished syncing {source_folder} to {target_folder} in {time.time() - start_time:.2f} seconds"
     )
-    for key, value in stats.items():
-        logging.info(f"{key}: {value}")
+
+
+# def _delete_outdated_element(target_path: Path):
+#     if target_path.is_file():
+#         target_path.unlink()
+#         return "deleted_file"
+#     elif target_path.is_dir():
+#         # TODO do own recursion for better tracking of progress, !!! accoutn for empty folders
+#         shutil.rmtree(target_path)
+#         return "deleted_folder"
+
+
+# def _copy_new_element(source_path: Path, target_path: Path):
+#     if source_path.is_file():
+#         shutil.copy2(source_path, target_path)
+#         return "copied_file"
+#     elif source_path.is_dir():
+#         # TODO do own recursion for better tracking of progress, !!! accoutn for empty folders
+#         shutil.copytree(source_path, target_path)
+#         return "copied_folder"
+
+
+# def _update_existing_file(
+#     source_path: Path, target_path: Path, file_hash_func: Callable
+# ):
+#     if file_hash_func(source_path) == file_hash_func(target_path):
+#         return "unchanged_file"
+#     target_path.unlink()
+#     shutil.copy2(source_path, target_path.parent)
+#     return "changed_file"
+
+
+# def aggregate_action_statistics(
+#     actions: List[Tuple[Callable, Any]]
+# ) -> Dict[str, List[Path]]:
+#     stats = {
+#         "deleted_folders": [],
+#         "deleted_files": [],
+#         "copied_folders": [],
+#         "copied_files": [],
+#         "updated_files": [],
+#     }
+#     for action, *args in actions:
+#         if action == _delete_outdated_element:
+#             p = args[0]
+#             if p.is_file():
+#                 stats["files_deleted"].append(args[0])
+#             elif p.is_dir():
+#                 stats["folders_deleted"].append(args[0].parent)
+#         elif action == _copy_new_element:
+#             stats["folders_copied"].append(args[1].parent)
+#         elif action == _update_existing_file:
+#             stats["files_updated"].append(args[1].parent)
+#         else:
+#             raise ValueError(f"Unknown action {action}")
